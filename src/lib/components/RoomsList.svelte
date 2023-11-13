3eae956b23c7b9f81123a1d650d9a12e263ab76f
Androix777 <androix77@gmail.com>
2023-11-13 20:49:39 +0500

    Rooms auto scroll

--- src/lib/components/RoomsList.svelte
+++ src/lib/components/RoomsList.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
-	import { mapStore, getDefaultRoom } from '../stores/MapStore';
+	import { onMount } from 'svelte';
+	import { mapStore, getDefaultRoom, selectedRoom } from '../stores/MapStore';
 	import RoomInfoPanel from './RoomInfoPanel.svelte';
 	import { Button } from 'flowbite-svelte';
 
+	let selectedRoomElement: HTMLElement;
+	let selectedRoomID = $selectedRoom;
 	$: rooms = $mapStore.rooms;
 
 	function addRoom() {
 		mapStore.addRoom(getDefaultRoom());
 	}
+
+	
+
+	onMount(() => {
+		if (selectedRoomElement) {
+			selectedRoomElement.scrollIntoView({ behavior: 'instant' });
+		}
+	});
 </script>
 
 <Button class="mb-6" on:click={addRoom}>Add new</Button>
 <div class="overflow-scroll" style="max-height: 800px">
 	{#each Object.values(rooms) as room (room.id)}
-		<div class="mb-4">
-			<RoomInfoPanel {room} />
-		</div>
+		{#if room.id == selectedRoomID}
+			<div class="mb-4" bind:this={selectedRoomElement}>
+				<RoomInfoPanel {room} />
+			</div>
+		{:else}
+			<div class="mb-4">
+				<RoomInfoPanel {room} />
+			</div>
+		{/if}
 	{/each}
 </div>
